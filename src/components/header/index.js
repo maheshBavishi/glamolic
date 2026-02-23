a55e00c27ce6efa566596aee104fd49b5924cfe1
mahesh <dev030.rejoice@gmail.com>
2026-02-23 12:16:22 +0530

    styles:header scroll effect added

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,39 @@
-'use client'
-import React, { useState } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from './header.module.scss';
 import classNames from 'classnames';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const Logo = '/assets/logo/logo.svg';
+
+function useScrollDirection() {
+    const [scrollDirection, setScrollDirection] = useState("noScroll");
+
+    useEffect(() => {
+        let lastScrollY = window.pageYOffset;
+        const updateScrollDirection = () => {
+            const currentScrollTop =
+                window.pageYOffset || document.documentElement.scrollTop;
+            const scrollY = window.pageYOffset;
+            const direction = scrollY > lastScrollY ? "topToDown" : "downToTop";
+            if (currentScrollTop === 0) {
+                setScrollDirection("noScroll");
+            } else if (
+                direction !== scrollDirection &&
+                (scrollY - lastScrollY > 10 || scrollY - lastScrollY < -10)
+            ) {
+                setScrollDirection(direction);
+            }
+            lastScrollY = scrollY > 0 ? scrollY : 0;
+        };
+        window.addEventListener("scroll", updateScrollDirection);
+        return () => {
+            window.removeEventListener("scroll", updateScrollDirection);
+        };
+    }, [scrollDirection]);
+
+    return scrollDirection;
+}
 
 const menuVariants = {
     open: {
@@ -47,11 +76,19 @@ const itemVariants = {
 };
 
 export default function Header() {
+    const scrollDirection = useScrollDirection();
     const [headerOpen, setHeaderOpen] = useState(false);
 
     return (
         <>
-            <header className={styles.header}>
+            <header className={classNames(
+                styles.header,
+                scrollDirection === "downToTop"
+                    ? styles.show
+                    : scrollDirection === "noScroll"
+                        ? null
+                        : styles.hide
+            )}>
                 <div className='container'>
                     <div className={styles.headerAlignment}>
                         <div className={styles.logo}>
@@ -119,4 +156,3 @@ export default function Header() {
         </>
     )
 }
-
