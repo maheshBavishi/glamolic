961ef390fb8981290171c59bd9814dd7524d12c3
SmitKakani7 <smit198.rejoice@gmail.com>
2026-04-08 10:55:23 +0530

    fixed graphql issue

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <GET_PAGE_SCHEMA />
+        {/* <GET_PAGE_SCHEMA /> */}
       </head>
       <body className={`${elMessiri.variable} ${heebo.variable}`}>
         <ClientProviders>
