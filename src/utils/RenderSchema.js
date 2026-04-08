8d972f1134e92eda8159f977fbac695d6bbc0d44
SmitKakani7 <smit198.rejoice@gmail.com>
2026-04-08 15:02:34 +0530

    fix: stringify JSON-LD schema in RenderSchema to prevent invalid script rendering

--- src/utils/RenderSchema.js
+++ src/utils/RenderSchema.js
@@ -7,21 +7,23 @@ export default function RenderSchema({ seoData }) {
         <script
           id="Organization_Schema"
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: seoData.Organization_Schema }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify(seoData.Organization_Schema),
+          }}
         />
       )}
       {seoData?.FAQ_Schema && (
         <script
           id="FAQ_Schema"
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: seoData.FAQ_Schema }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(seoData.FAQ_Schema) }}
         />
       )}
       {seoData?.BreadcrumbList_Schema && (
         <script
           id="BreadcrumbList_Schema"
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: seoData.BreadcrumbList_Schema }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(seoData.BreadcrumbList_Schema) }}
         />
       )}
     </>
